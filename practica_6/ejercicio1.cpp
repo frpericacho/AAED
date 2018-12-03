f7c5c6ac7c59c5bcdc0b98a1ee1de723873cef77
frpericacho <frpericacho@gmail.com>
2018-12-03 11:04:22 +0100

    :rice_ball:

--- practica_6/ejercicio1.cpp
+++ practica_6/ejercicio1.cpp
@@ -3,9 +3,12 @@
 #include "PILA_ENLA.h"
 using namespace std;
 
-void rellenaC(Pila<int> );
-void rellenaP(Cola<int> );
-void fun(Pila<int> ,Cola<int> );
+void rellenaP(Pila<int> &);
+void rellenaC(Cola<int> &);
+//void imprimeC(Cola<int> &);
+//
+void imprimeP(Pila<int> );
+void fun(Pila<int> ,Cola<int> &);
 
 int main(int argc, char const *argv[])
 {
@@ -15,45 +18,72 @@ int main(int argc, char const *argv[])
     rellenaP(p);
     rellenaC(c);
 
+    //imprimeP(p);
+    //imprimeC(c);
+
     fun(p ,c);
 
     return 0;
 }
 
-void fun(Pila<int> p,Cola<int> c){
-    int auxp = 0,auxc = 0,i = 1;
+/*void imprimeC(Cola<int> &c){
+    while(!c.vacia()){
+        cout << c.frente() << endl;
+        c.pop();
+    }
+}
+
+void imprimeP(Pila<int> p){
+    while(!p.vacia()){
+        cout << p.tope() << endl;
+        p.pop();
+    }
+}*/
+
+void fun(Pila<int> p,Cola<int> &c){
+    int auxp = 0,auxc = 0,i = 1,cont = 0;
     while(!p.vacia() || !c.vacia()){
         if(i%3 == 0){
-            if(auxc!=auxp)
+            if(auxc!=auxp){
+                cout << cont << endl;
+                cout << "no son iguales " << endl;
                 exit(0);
+            }
         }
         if(i%2 == 0){
             auxp = p.tope();
             p.pop();
+            c.pop();
             i++;
+            cont++;
         }else{
             auxc = c.frente();
             c.pop();
+            p.pop();
             i++;
+            cont++;
         }
     }
     cout << "son isomorficas" << endl;
 }
 
-void rellenaP(Pila<int> p){
+void rellenaP(Pila<int> &p){
     int aux = 0;
     cout << "introduzcalos numeros de la pila('-1' para salir)" << endl;
+    cout << "ten encuenta que debes meterlos desde el final al principio" << endl;
     while(aux!= -1){
         cin >> aux;
-        p.push(aux);
+        if(aux!=-1)
+            p.push(aux);
     }
 }
 
-void rellenaC(Cola<int> c){
+void rellenaC(Cola<int> &c){
     int aux = 0;
     cout << "introduzcalos numeros de la cola('-1' para salir)" << endl;
     while(aux!= -1){
         cin >> aux;
-        c.push(aux);
+        if(aux!=-1)
+            c.push(aux);
     }
 }
